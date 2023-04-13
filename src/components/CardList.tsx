3383a824ca72a8dae651f7a51a9985c3304d7ebb
adilawan1 <1ahmed2adil3awan@gmail.com>
2023-04-14 03:29:55 +0500

    Adding key attr

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -18,8 +18,9 @@ const CardList = ({ ItemCard_List, title }: ItemList_Props) => {
       <div className="container mx-auto">
         <div className="text-5xl m-4 font-bold text-center">{title}</div>
         <div className="grid md:grid-cols-4 justify-center">
-          {ItemCard_List.map((Card) => (
+          {ItemCard_List.map((Card, index) => (
             <ItemCard
+              key={index}
               title={Card.title}
               price={Card.price}
               picture={Card.picture}
